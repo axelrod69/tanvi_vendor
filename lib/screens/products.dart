8f5eff807c6e7ccb6b1dc64b967cbcfe41a6ddbc
axelrod69 <siddc.8@gmail.com>
2022-04-22 01:09:24 +0530

    addProducts

--- lib/screens/products.dart
+++ lib/screens/products.dart
@@ -3,11 +3,41 @@ import 'package:flutter/material.dart';
 class Products extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+
     // TODO: implement build
     return Scaffold(
-      body: Center(
+      body: const Center(
         child: Text('Products'),
       ),
+      floatingActionButton: Container(
+        margin: EdgeInsets.only(bottom: height * 0.07),
+        decoration: const BoxDecoration(shape: BoxShape.circle, boxShadow: [
+          BoxShadow(color: Colors.grey, blurRadius: 8, offset: Offset(2, 2))
+        ]),
+        child: CircleAvatar(
+          radius: width * 0.07,
+          backgroundColor: Colors.green,
+          child: const Text(
+            '+',
+            style: TextStyle(color: Colors.white, fontSize: 35),
+          ),
+        ),
+      ),
+      // floatingActionButton: Container(
+      //   width: width * 0.2,
+      //   height: height * 0.06,
+      //   margin: EdgeInsets.only(bottom: height * 0.07),
+      //   decoration: BoxDecoration(
+      //       color: Colors.green,
+      //       shape: BoxShape.circle,
+      //       boxShadow: [
+      //         BoxShadow(
+      //             color: Colors.grey, blurRadius: 10, offset: Offset(1, 2))
+      //       ]),
+      // ),
+      // floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 }
